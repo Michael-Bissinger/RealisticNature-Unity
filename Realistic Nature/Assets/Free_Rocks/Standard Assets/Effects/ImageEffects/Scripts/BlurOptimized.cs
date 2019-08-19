39488d38323201917a8f917867773e0958e32704
Michael-Bissinger <Michael.Bissinger@inb.de>
2019-08-19 13:55:24 +0200

    add scripts for nature

--- Realistic Nature/Assets/Free_Rocks/Standard Assets/Effects/ImageEffects/Scripts/BlurOptimized.cs
+++ Realistic Nature/Assets/Free_Rocks/Standard Assets/Effects/ImageEffects/Scripts/BlurOptimized.cs
@@ -1,93 +1,89 @@
-using System;
 using UnityEngine;
 
-namespace UnityStandardAssets.ImageEffects
+[ExecuteInEditMode]
+[RequireComponent (typeof(Camera))]
+[AddComponentMenu ("Image Effects/Blur/Blur (Optimized)")]
+public class BlurOptimized : PostEffectsBase
 {
-    [ExecuteInEditMode]
-    [RequireComponent (typeof(Camera))]
-    [AddComponentMenu ("Image Effects/Blur/Blur (Optimized)")]
-    public class BlurOptimized : PostEffectsBase
-    {
 
-        [Range(0, 2)]
-        public int downsample = 1;
+	[Range(0, 2)]
+	public int downsample = 1;
 
-        public enum BlurType {
-            StandardGauss = 0,
-            SgxGauss = 1,
-        }
+	public enum BlurType {
+		StandardGauss = 0,
+		SgxGauss = 1,
+	}
 
-        [Range(0.0f, 10.0f)]
-        public float blurSize = 3.0f;
+	[Range(0.0f, 10.0f)]
+	public float blurSize = 3.0f;
+	
+	[Range(1, 4)]
+	public int blurIterations = 2;
 
-        [Range(1, 4)]
-        public int blurIterations = 2;
+	public BlurType blurType= BlurType.StandardGauss;
 
-        public BlurType blurType= BlurType.StandardGauss;
-
-        public Shader blurShader = null;
-        private Material blurMaterial = null;
+	public Shader blurShader = null;
+	private Material blurMaterial = null;
 
 
-        public override bool CheckResources () {
-            CheckSupport (false);
+	public override bool CheckResources () {
+		CheckSupport (false);
+	
+		blurMaterial = CheckShaderAndCreateMaterial (blurShader, blurMaterial);
+		
+		if (!isSupported)
+			ReportAutoDisable ();
+		return isSupported;
+	}
 
-            blurMaterial = CheckShaderAndCreateMaterial (blurShader, blurMaterial);
+	public void OnDisable () {
+		if (blurMaterial)
+			DestroyImmediate (blurMaterial);
+	}
 
-            if (!isSupported)
-                ReportAutoDisable ();
-            return isSupported;
-        }
+	public void OnRenderImage (RenderTexture source, RenderTexture destination) {
+		if (CheckResources() == false) {
+			Graphics.Blit (source, destination);
+			return;
+		}
 
-        public void OnDisable () {
-            if (blurMaterial)
-                DestroyImmediate (blurMaterial);
-        }
+		float widthMod = 1.0f / (1.0f * (1<<downsample));
 
-        public void OnRenderImage (RenderTexture source, RenderTexture destination) {
-            if (CheckResources() == false) {
-                Graphics.Blit (source, destination);
-                return;
-            }
+		blurMaterial.SetVector ("_Parameter", new Vector4 (blurSize * widthMod, -blurSize * widthMod, 0.0f, 0.0f));
+		source.filterMode = FilterMode.Bilinear;
 
-            float widthMod = 1.0f / (1.0f * (1<<downsample));
+		int rtW = source.width >> downsample;
+		int rtH = source.height >> downsample;
 
-            blurMaterial.SetVector ("_Parameter", new Vector4 (blurSize * widthMod, -blurSize * widthMod, 0.0f, 0.0f));
-            source.filterMode = FilterMode.Bilinear;
+		// downsample
+		RenderTexture rt = RenderTexture.GetTemporary (rtW, rtH, 0, source.format);
 
-            int rtW = source.width >> downsample;
-            int rtH = source.height >> downsample;
+		rt.filterMode = FilterMode.Bilinear;
+		Graphics.Blit (source, rt, blurMaterial, 0);
 
-            // downsample
-            RenderTexture rt = RenderTexture.GetTemporary (rtW, rtH, 0, source.format);
+		var passOffs= blurType == BlurType.StandardGauss ? 0 : 2;
+		
+		for(int i = 0; i < blurIterations; i++) {
+			float iterationOffs = (i*1.0f);
+			blurMaterial.SetVector ("_Parameter", new Vector4 (blurSize * widthMod + iterationOffs, -blurSize * widthMod - iterationOffs, 0.0f, 0.0f));
 
-            rt.filterMode = FilterMode.Bilinear;
-            Graphics.Blit (source, rt, blurMaterial, 0);
+			// vertical blur
+			RenderTexture rt2 = RenderTexture.GetTemporary (rtW, rtH, 0, source.format);
+			rt2.filterMode = FilterMode.Bilinear;
+			Graphics.Blit (rt, rt2, blurMaterial, 1 + passOffs);
+			RenderTexture.ReleaseTemporary (rt);
+			rt = rt2;
 
-            var passOffs= blurType == BlurType.StandardGauss ? 0 : 2;
+			// horizontal blur
+			rt2 = RenderTexture.GetTemporary (rtW, rtH, 0, source.format);
+			rt2.filterMode = FilterMode.Bilinear;
+			Graphics.Blit (rt, rt2, blurMaterial, 2 + passOffs);
+			RenderTexture.ReleaseTemporary (rt);
+			rt = rt2;
+		}
+		
+		Graphics.Blit (rt, destination);
 
-            for(int i = 0; i < blurIterations; i++) {
-                float iterationOffs = (i*1.0f);
-                blurMaterial.SetVector ("_Parameter", new Vector4 (blurSize * widthMod + iterationOffs, -blurSize * widthMod - iterationOffs, 0.0f, 0.0f));
-
-                // vertical blur
-                RenderTexture rt2 = RenderTexture.GetTemporary (rtW, rtH, 0, source.format);
-                rt2.filterMode = FilterMode.Bilinear;
-                Graphics.Blit (rt, rt2, blurMaterial, 1 + passOffs);
-                RenderTexture.ReleaseTemporary (rt);
-                rt = rt2;
-
-                // horizontal blur
-                rt2 = RenderTexture.GetTemporary (rtW, rtH, 0, source.format);
-                rt2.filterMode = FilterMode.Bilinear;
-                Graphics.Blit (rt, rt2, blurMaterial, 2 + passOffs);
-                RenderTexture.ReleaseTemporary (rt);
-                rt = rt2;
-            }
-
-            Graphics.Blit (rt, destination);
-
-            RenderTexture.ReleaseTemporary (rt);
-        }
-    }
+		RenderTexture.ReleaseTemporary (rt);
+	}
 }
